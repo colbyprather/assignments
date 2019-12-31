eb06d2265b74994276f10497f62897c4bcb0506c
Colby Prather <colbynash01@gmail.com>
2019-12-31 12:42:44 -0700

    profile added slideshow needs styling

--- projects/portfolio/src/Home.js
+++ projects/portfolio/src/Home.js
@@ -1,72 +1,98 @@
 import React, { Component } from 'react';
 import "./homeStyles.css"
+import makeCarousel from 'react-reveal/makeCarousel';
+import Slide from 'react-reveal/Slide';
+import styled, { css } from 'styled-components';
 import Vtrade from "./vtrade.jpg"
 import Vtrade2 from "./vtrade2.jpg"
 import Vtrade3 from "./vtrade3.jpg"
 
 class Home extends Component {
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+    
     render () {
+        const width = '300px', height='150px';
+        const Container = styled.div`
+        border: 1px solid black;
+        margin: 10px 10px 10px 15%;
+        position: relative;
+        overflow: hidden;
+        width: ${width};
+        `;
+        const Children  = styled.div`
+        width: ${width};
+        position: relative;
+        height: 175px;
+        `;
+        const Arrow = styled.div`
+        text-shadow: 1px 1px 1px #fff;
+        z-index: 100;
+        line-height: ${height};
+        text-align: center;
+        position: absolute;
+        top: 0;
+        width: 10%;
+        font-size: 3em;
+        cursor: pointer;
+        user-select: none;
+        color: black;
+        ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
+        `;
+        const Dot = styled.span`
+        font-size: 1.5em;
+        cursor: pointer;
+        text-shadow: 1px 1px 1px #fff;
+        user-select: none;
+        `;
+        const Dots = styled.span`
+        text-align: center;
+        width: ${width};
+        z-index: 100;
+        `;
+        const CarouselUI = ({ position, total, handleClick, children }) => (
+        <Container>
+            <Children>
+            {children}
+            <Arrow onClick={handleClick} data-position={position - 1}>{'<'}</Arrow>
+            <Arrow right onClick={handleClick} data-position={position + 1}>{'>'}</Arrow>
+            </Children>
+            <Dots>
+            {Array(...Array(total)).map( (val, index) =>
+                <Dot key={index} onClick={handleClick} data-position={index}>
+                {index === position ? '● ' : '○ ' }
+                </Dot>
+            )}
+            </Dots>
+        </Container>
+        );
+        const Carousel = makeCarousel(CarouselUI);
         return (
             <div>
                 <h1 className="textTop">Projects</h1>
                 <div className="layout">
                     <div className="boxHolder">
-                        <div class="slideshow-container">
-                            <div class="mySlides fade">
-                            <div class="numbertext">1 / 3</div>
-                            <img src={Vtrade} alt="Home Page" style={{width: "100%"}}></img>
-                            <div class="text">Caption Text</div>
+                    <Carousel>
+                        <Slide right>
+                        <div>
+                            <img className="images" src={Vtrade} alt="screenshot"></img>
+                            {/* <h1>Slide 1</h1>
+                            <p>Slide Description</p> */}
                         </div>
-
-                        <div class="mySlides fade">
-                            <div class="numbertext">2 / 3</div>
-                            <img src={Vtrade2} alt="Camping Page" style={{width: "100%"}}></img>
-                            <div class="text">Caption Two</div>
+                        </Slide>
+                        <Slide right>
+                        <div>
+                            <img className="images" src={Vtrade2} alt="screenshot"></img>
+                            {/* <h1>Slide 2</h1>
+                            <p>Slide Description</p> */}
                         </div>
-
-                        <div class="mySlides fade">
-                            <div class="numbertext">3 / 3</div>
-                            <img src={Vtrade3} alt="Posting Page" style={{width: "100%"}}></img>
-                            <div class="text">Caption Three</div>
+                        </Slide>
+                        <Slide right>
+                        <div>
+                            <img className="images" src={Vtrade3} alt="screenshot"></img>
+                            {/* <h1>Slide 3</h1>
+                            <p>Slide Description</p> */}
                         </div>
-                            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                            <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                        </div>
-                        <br/>
-                        <div style={{textAlign: "center"}}>
-                            <span class="dot" onclick="currentSlide(1)"></span>
-                            <span class="dot" onclick="currentSlide(2)"></span>
-                            <span class="dot" onclick="currentSlide(3)"></span>
-                        </div>
+                        </Slide>
+                    </Carousel>
                         {/* <img className="images" src={Vtrade} alt="screenshot"></img> */}
                         <a className="viewButton" href={"https://vtrade.herokuapp.com/"} target="_blank" rel="noopener noreferrer">View</a>
                     </div>
