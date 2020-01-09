d56529fbaa7d7646f33e4c96aa4648d1834f8a4a
Colby Prather <colbynash01@gmail.com>
2020-01-08 23:59:39 -0700

    added more to about page

--- projects/portfolio/src/About.js
+++ projects/portfolio/src/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './aboutStyles.css';
+import HeadShot from "./headshot.JPG"
 
 const About = () => {
     return (
         <div className="gridLayout">
             <div className='picContainer'>
-                <img className='picOfMe' src="http://www.focustalentmgmt.com/uploads/9/4/0/4/9404157/colby-prather-main_orig.jpg" alt='Me'></img>
+                <img className='picOfMe' src={HeadShot} alt='Me'></img>
             </div>
             <div style={{paddingBottom: "20px"}} className="textContainer">
                 <p className="ptext">I'm Colby Prather. I'm the type of person that likes to build something and be able to see it. With web design I'm able to do both. I love the challenge when trying to design somthing new that I've never done before learning new things and with web development theres always something new to learn. Then being able to see the final project after putting in all that work and knowing that so many other people will too.</p>
